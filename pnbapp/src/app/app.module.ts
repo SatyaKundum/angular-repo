190726de7e19fa66cd2f61e02e93b2ba9a9e047e
SatyaKundum <SatyaKundum@gmail.com>
2019-03-25 14:19:37 +0530

    new components

--- pnbapp/src/app/app.module.ts
+++ pnbapp/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ServerComponent} from './server/server.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { PasswordComponent } from './password/password.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    LoginComponent,
+    RegisterComponent,
+    PasswordComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
